39985f7437cba9f770bbe7b4228ab023a4301c92
agum.ruswandi10@gmail.com <agum.ruswandi10@gmail.com>
2024-12-03 13:32:01 +0700

    update instansiasi object

--- Bank_XYZ/cypress/integration/addCustomer.test.js
+++ Bank_XYZ/cypress/integration/addCustomer.test.js
@@ -2,37 +2,37 @@
 import BankingPage from "../support/pages/BankingPage";
 
 describe('Menambah Customer ', () => {
-    const managerPage = new BankingPage();
+    const Banking = new BankingPage();
 
     beforeEach(() => {
-        managerPage.visit();
-        managerPage.navigateToManagerLogin();
+        Banking.visit();
+        Banking.navigateToManagerLogin();
     });
 
     it('Penambahan Customer Baru', () => {
-        managerPage.clickAddCustomer();
-        managerPage.fillCustomerDetails('agum', 'ruswandi', 'E12456');
-        managerPage.submitCustomerForm();
-        managerPage.verifyAlertText('Customer added successfully');
+        Banking.clickAddCustomer();
+        Banking.fillCustomerDetails('agum', 'ruswandi', 'E12456');
+        Banking.submitCustomerForm();
+        Banking.verifyAlertText('Customer added successfully');
     });
 
     it('Validasi penambahan duplikat customer', () => {
 
         //menambah custommer pertama
-        managerPage.clickAddCustomer();
-        managerPage.fillCustomerDetails('agum','ruswandi','E12456')
-        managerPage.submitCustomerForm();
+        Banking.clickAddCustomer();
+        Banking.fillCustomerDetails('agum','ruswandi','E12456')
+        Banking.submitCustomerForm();
        
-        managerPage.verifyAlertText('Customer added successfully');
+        Banking.verifyAlertText('Customer added successfully');
 
         //menambah customer ke dua
-        managerPage.clickAddCustomer();
-        managerPage.fillCustomerDetails1('agum','ruswandi','E12456');
-        managerPage.submitCustomerForm();
+        Banking.clickAddCustomer();
+        Banking.fillCustomerDetails1('agum','ruswandi','E12456');
+        Banking.submitCustomerForm();
 
         //validasi penambahan duplikat Customer
-        managerPage.navigateToCustomer();
-        managerPage.searchCustomer('agum');
+        Banking.navigateToCustomer();
+        Banking.searchCustomer('agum');
 
         // Verifikasi bahwa "agum" muncul di tabel (dalam kolom First Name)
         cy.get('table').contains('td', 'agum').should('exist');
@@ -40,36 +40,36 @@ describe('Menambah Customer ', () => {
     });
 
     it('Validasi Penambahan Customer dengan hanya Firstname', () => {
-        managerPage.clickAddCustomer();
-        managerPage.fillCustomerDetails1('agum', '', '');
-        managerPage.submitCustomerForm();
+        Banking.clickAddCustomer();
+        Banking.fillCustomerDetails1('agum', '', '');
+        Banking.submitCustomerForm();
         cy.get('input[ng-model="lName"]').should('have.prop', 'validationMessage', 'Please fill out this field.');
         cy.get('input[ng-model="postCd"]').should('have.prop', 'validationMessage', 'Please fill out this field.');
 
     });
 
     it('Validasi Penambahan Customer dengan hanya Lastname', () => {
-        managerPage.clickAddCustomer();
-        managerPage.fillCustomerDetails1('', 'ruswandi', '');
-        managerPage.submitCustomerForm();
+        Banking.clickAddCustomer();
+        Banking.fillCustomerDetails1('', 'ruswandi', '');
+        Banking.submitCustomerForm();
         cy.get('input[ng-model="fName"]').should('have.prop', 'validationMessage', 'Please fill out this field.');
         cy.get('input[ng-model="postCd"]').should('have.prop', 'validationMessage', 'Please fill out this field.');
 
     });
 
     it('Validasi Penambahan Customer dengan hanya Postcode', () => {
-        managerPage.clickAddCustomer();
-        managerPage.fillCustomerDetails1('', '', '123456');
-        managerPage.submitCustomerForm();
+        Banking.clickAddCustomer();
+        Banking.fillCustomerDetails1('', '', '123456');
+        Banking.submitCustomerForm();
         cy.get('input[ng-model="fName"]').should('have.prop', 'validationMessage', 'Please fill out this field.');
         cy.get('input[ng-model="lName"]').should('have.prop', 'validationMessage', 'Please fill out this field.');
 
     });
     
     it('Validasi Penambahan Customer dengan field kosong', () => {
-        managerPage.clickAddCustomer();
-        managerPage.fillCustomerDetails1('', '', '');
-        managerPage.submitCustomerForm();
+        Banking.clickAddCustomer();
+        Banking.fillCustomerDetails1('', '', '');
+        Banking.submitCustomerForm();
         cy.get('input[ng-model="fName"]').should('have.prop', 'validationMessage', 'Please fill out this field.');
         cy.get('input[ng-model="lName"]').should('have.prop', 'validationMessage', 'Please fill out this field.');
         cy.get('input[ng-model="postCd"]').should('have.prop', 'validationMessage', 'Please fill out this field.');
@@ -77,64 +77,64 @@ describe('Menambah Customer ', () => {
 
 
     it('Validasi Penambahan Customer FirstName dengan kombinasi karakter & number', () => {
-        managerPage.clickAddCustomer();
-        managerPage.fillCustomerDetails1('@g+*&^^123', 'ruswandi', '123456');
-        managerPage.submitCustomerForm();
-        managerPage.verifyAlertText('Customer added successfully');
+        Banking.clickAddCustomer();
+        Banking.fillCustomerDetails1('@g+*&^^123', 'ruswandi', '123456');
+        Banking.submitCustomerForm();
+        Banking.verifyAlertText('Customer added successfully');
     });
 
     it('Validasi Penambahan Customer LastName dengan kombinasi karakter & number', () => {
-        managerPage.clickAddCustomer();
-        managerPage.fillCustomerDetails('agum', '12r-_-$@#', 'E1234589');
-        managerPage.submitCustomerForm();
-        managerPage.verifyAlertText('Customer added successfully');
+        Banking.clickAddCustomer();
+        Banking.fillCustomerDetails('agum', '12r-_-$@#', 'E1234589');
+        Banking.submitCustomerForm();
+        Banking.verifyAlertText('Customer added successfully');
     });
 
     it('Validasi Penambahan Customer postcode dengan kombinasi karakter & number', () => {
-        managerPage.clickAddCustomer();
-        managerPage.fillCustomerDetails('agum', 'ruswandi', 'E123+*&%$');
-        managerPage.submitCustomerForm();
-        managerPage.verifyAlertText('Customer added successfully');
+        Banking.clickAddCustomer();
+        Banking.fillCustomerDetails('agum', 'ruswandi', 'E123+*&%$');
+        Banking.submitCustomerForm();
+        Banking.verifyAlertText('Customer added successfully');
     });
 
     it('Validasi Penambahan Customer semua kolom dengan kombinasi karakter & number', () => {
-        managerPage.clickAddCustomer();
-        managerPage.fillCustomerDetails('@g%$#@12', 'r%^##@#%124', 'E123+*&%$');
-        managerPage.submitCustomerForm();
-        managerPage.verifyAlertText('Customer added successfully');
+        Banking.clickAddCustomer();
+        Banking.fillCustomerDetails('@g%$#@12', 'r%^##@#%124', 'E123+*&%$');
+        Banking.submitCustomerForm();
+        Banking.verifyAlertText('Customer added successfully');
     });
 
     it('Validasi Input Field First Name dengan Huruf Kapital', () => {
-        managerPage.clickAddCustomer();
-        managerPage.fillCustomerDetails('AGUM','ruswandi','w123456');
-        managerPage.submitCustomerForm();
-        managerPage.verifyAlertText('Customer added successfully')
+        Banking.clickAddCustomer();
+        Banking.fillCustomerDetails('AGUM','ruswandi','w123456');
+        Banking.submitCustomerForm();
+        Banking.verifyAlertText('Customer added successfully')
     });
 
     it('Validasi Input Last  Name dengan Huruf Kapital', () => {
-        managerPage.clickAddCustomer();
-        managerPage.fillCustomerDetails('agum','RUSWANDI','w123456');
-        managerPage.submitCustomerForm();
-        managerPage.verifyAlertText('Customer added successfully')
+        Banking.clickAddCustomer();
+        Banking.fillCustomerDetails('agum','RUSWANDI','w123456');
+        Banking.submitCustomerForm();
+        Banking.verifyAlertText('Customer added successfully')
     });
 
     it('Validasi Input Field Postcode dengan Huruf Kapital', () => {
-        managerPage.clickAddCustomer();
-        managerPage.fillCustomerDetails('agum','ruswandi','AZSDFGH');
-        managerPage.submitCustomerForm();
-        managerPage.verifyAlertText('Customer added successfully')
+        Banking.clickAddCustomer();
+        Banking.fillCustomerDetails('agum','ruswandi','AZSDFGH');
+        Banking.submitCustomerForm();
+        Banking.verifyAlertText('Customer added successfully')
     });
 
     it('Validasi Input Seluruh Field  dengan Huruf Kapital', () => {
-        managerPage.clickAddCustomer();
-        managerPage.fillCustomerDetails('AGUM','RUSWANDI','AZSDFGH');
-        managerPage.submitCustomerForm();
-        managerPage.verifyAlertText('Customer added successfully')
+        Banking.clickAddCustomer();
+        Banking.fillCustomerDetails('AGUM','RUSWANDI','AZSDFGH');
+        Banking.submitCustomerForm();
+        Banking.verifyAlertText('Customer added successfully')
     });
 
     it('Validasi pengujian tombol Home di addCustomer', () => {
-        managerPage.clickAddCustomer();
-        managerPage.navigateTohome();
+        Banking.clickAddCustomer();
+        Banking.navigateTohome();
 
         //assertion
         cy.get('button[ng-click="home()"]').should('exist');
