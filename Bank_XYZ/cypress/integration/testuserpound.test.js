029c358f2a013361c2294303adf7a196e2ff1872
agum.ruswandi10@gmail.com <agum.ruswandi10@gmail.com>
2024-12-11 20:19:00 +0700

    update penambahan testcase & penambahan file testcase

--- Bank_XYZ/cypress/integration/testuserpound.test.js
+++ Bank_XYZ/cypress/integration/testuserpound.test.js
@@ -1,49 +1,48 @@
 /// <reference types="cypress"/>
 import  BankingPage from "../support/pages/BankingPage";
 
-describe('Validasi Fitur User di rekening Dollar ', () => {
+describe('Validasi Fitur User di rekening Pound', () => {
     const Banking = new BankingPage;
     beforeEach(() => {
         Banking.visit();
         Banking.navigateToCustomerlogin();
     });
 
-
-    it('Harus menampilkan pesan error saat deposit menggunakan huruf di rekening Dollar', () => {
+    it('Harus menampilkan pesan error saat deposit menggunakan huruf di rekening Pound', () => {
         //memilih user
         cy.log('Memilih user')
         Banking.selectuser('Harry Potter');
         Banking.loginusername();
        
-        //melakukan deposit rekening dollar
+        //melakukan deposit rekening Pound
         cy.log('memilih rekening');
-        Banking.selectaccount('1004');
+        Banking.selectaccount('1005');
         //assertion
         cy.contains('Harry Potter').should('be.visible');
-        cy.contains('Account Number : 1004').should('be.visible');
-        cy.contains('Currency : Dollar').should('be.visible');
+        cy.contains('Account Number : 1005').should('be.visible');
+        cy.contains('Currency : Pound').should('be.visible');
         
         Banking.deposit();
         cy.log('memasukan jumlah');
         Banking.enteramount('abnmci');
         Banking.submitdeposit();
         cy.get('input[ng-model="amount"]').should('have.prop', 'validationMessage', 'Please fill out this field.');
-
     });
 
-    it('Harus menampilkan pesan error saat deposit menggunakan simbol di rekening Dollar', () => {
+
+    it('Harus menampilkan pesan error saat deposit menggunakan simbol di rekening Pound', () => {
          //memilih user
          cy.log('Memilih user')
          Banking.selectuser('Harry Potter');
          Banking.loginusername();
         
-         //melakukan deposit rekening dollar
+         //melakukan deposit rekening Pound
          cy.log('memilih rekening');
-         Banking.selectaccount('1004');
+         Banking.selectaccount('1005');
          //assertion
          cy.contains('Harry Potter').should('be.visible');
-         cy.contains('Account Number : 1004').should('be.visible');
-         cy.contains('Currency : Dollar').should('be.visible');
+         cy.contains('Account Number : 1005').should('be.visible');
+         cy.contains('Currency : Pound').should('be.visible');
          
          Banking.deposit();
          cy.log('memasukan jumlah');
@@ -52,72 +51,68 @@ describe('Validasi Fitur User di rekening Dollar ', () => {
          cy.get('input[ng-model="amount"]').should('have.prop', 'validationMessage', 'Please fill out this field.');
     });
 
-
-    it('Harus Tidak Ada Respons Saat Deposit Menggunakan Nilai minus  di rekening Dollar', () => {
-        //memilih user
-        cy.log('Memilih user')
-        Banking.selectuser('Harry Potter');
-        Banking.loginusername();
-       
-        //melakukan deposit rekening dollar
-        cy.log('memilih rekening');
-        Banking.selectaccount('1004');
-        //assertion
-        cy.contains('Harry Potter').should('be.visible');
-        cy.contains('Account Number : 1004').should('be.visible');
-        cy.contains('Currency : Dollar').should('be.visible');
+    it('Harus Tidak Ada Respons Saat Deposit Menggunakan Nilai minus  di rekening Pound', () => {
+         //memilih user
+         cy.log('Memilih user')
+         Banking.selectuser('Harry Potter');
+         Banking.loginusername();
         
-        Banking.deposit();
-        cy.log('memasukan jumlah');
-        Banking.enteramount('-1000');
-        Banking.submitdeposit();
-        cy.get('button[type="submit"]').should('not.be.disabled');
-       
-   });
+         //melakukan deposit rekening dollar
+         cy.log('memilih rekening');
+         Banking.selectaccount('1005');
+         //assertion
+         cy.contains('Harry Potter').should('be.visible');
+         cy.contains('Account Number : 1005').should('be.visible');
+         cy.contains('Currency : Pound').should('be.visible');
+         
+         Banking.deposit();
+         cy.log('memasukan jumlah');
+         Banking.enteramount('-1000');
+         Banking.submitdeposit();
+         cy.get('button[type="submit"]').should('not.be.disabled');
+    });
 
-
-   it('Harus menampilkan pesan error saat deposit menggunakan huruf dan angka di rekening Dollar', () => {
-    //memilih user
+    it('Harus menampilkan pesan error saat deposit menggunakan huruf dan angka di rekening Pound', () => {
+        //memilih user
     cy.log('Memilih user')
     Banking.selectuser('Harry Potter');
     Banking.loginusername();
    
     //melakukan deposit rekening dollar
     cy.log('memilih rekening');
-    Banking.selectaccount('1004');
+    Banking.selectaccount('1005');
     //assertion
     cy.contains('Harry Potter').should('be.visible');
-    cy.contains('Account Number : 1004').should('be.visible');
-    cy.contains('Currency : Dollar').should('be.visible');
+    cy.contains('Account Number : 1005').should('be.visible');
+    cy.contains('Currency : Pound').should('be.visible');
     
     Banking.deposit();
     cy.log('memasukan jumlah');
     Banking.enteramount('100adfgh');
     Banking.submitdeposit();
     cy.get('input[ng-model="amount"]').should('have.prop', 'validationMessage', 'Please fill out this field.');
-});
-
-
-    it('Harus berhasil menambahkan nilai ke rekening dollar', () => {
-        //memilih user
-        cy.log('Memilih user')
-        Banking.selectuser('Harry Potter');
-        Banking.loginusername();
-
-        //melakukan deposit rekening dollar
-        cy.log('memilih rekening');
-        Banking.selectaccount('1004');
-        Banking.deposit();
-        cy.log('memasukan jumlah');
-        Banking.enteramount('5000');
-        Banking.submitdeposit();
-        //assertion
-        cy.log('muncul assertion');
-        cy.contains('span', 'Deposit Successful').should('be.visible');
-
     });
+
+    it('Harus berhasil menambahkan nilai ke rekening Pound', () => {
+         //memilih user
+         cy.log('Memilih user')
+         Banking.selectuser('Harry Potter');
+         Banking.loginusername();
+ 
+         //melakukan deposit rekening dollar
+         cy.log('memilih rekening');
+         Banking.selectaccount('1005');
+         Banking.deposit();
+         cy.log('memasukan jumlah');
+         Banking.enteramount('6000');
+         Banking.submitdeposit();
+         //assertion
+         cy.log('muncul assertion');
+         cy.contains('span', 'Deposit Successful').should('be.visible');
+    });
+
     
-    it('Harus muncul pesan error saat withdrawl menggunakan huruf di rekening Dollar', () => {
+    it('Harus muncul pesan error saat withdrawl menggunakan huruf di rekening Pound', () => {
          //memilih user
          cy.log('Memilih user')
          Banking.selectuser('Harry Potter');
@@ -125,7 +120,7 @@ describe('Validasi Fitur User di rekening Dollar ', () => {
 
          //memilih rekening
          cy.log('memilih rekening');
-         Banking.selectaccount('1004');
+         Banking.selectaccount('1005');
 
         //masuk menu withdrawl
          Banking.withdrawl();
@@ -133,11 +128,10 @@ describe('Validasi Fitur User di rekening Dollar ', () => {
         cy.log('memasukan jumlah');
         Banking.enterwithdrawl('affffk');
         cy.get('input[ng-model="amount"]').should('have.prop', 'validationMessage', 'Please fill out this field.');
-
     });
 
-
-    it('Harus muncul pesan error saat withdrawl mengunakan simbol di rekening dollar ', () => {
+    
+    it('Harus muncul pesan error saat withdrawl mengunakan simbol di rekening Pound', () => {
         //memilih user
         cy.log('Memilih user')
         Banking.selectuser('Harry Potter');
@@ -145,7 +139,7 @@ describe('Validasi Fitur User di rekening Dollar ', () => {
 
         //memilih rekening
         cy.log('memilih rekening');
-        Banking.selectaccount('1004');
+        Banking.selectaccount('1005');
 
         //masuk menu withdrawl
         Banking.withdrawl();
@@ -155,19 +149,36 @@ describe('Validasi Fitur User di rekening Dollar ', () => {
         cy.get('input[ng-model="amount"]').should('have.prop', 'validationMessage', 'Please fill out this field.');
     });
 
+    it('Harus muncul pesan error saat withdrawl mengunakan simbol di rekening Pound', () => {
+         //memilih user
+         cy.log('Memilih user')
+         Banking.selectuser('Harry Potter');
+         Banking.loginusername();
+ 
+         //memilih rekening
+         cy.log('memilih rekening');
+         Banking.selectaccount('1004');
+ 
+         //masuk menu withdrawl
+         Banking.withdrawl();
+         cy.contains('Amount to be Withdrawn :').should('be.visible');
+         cy.log('memasukan jumlah');
+         Banking.enterwithdrawl('@#$%^&*');
+         cy.get('input[ng-model="amount"]').should('have.prop', 'validationMessage', 'Please fill out this field.');
+    });
 
     it('Harus Tidak Ada Respons Saat withdrawl Menggunakan Nilai minus  di rekening Dollar', () => {
-        cy.log('Memilih user')
+        cy.log('Memilih user');
         Banking.selectuser('Harry Potter');
         Banking.loginusername();
        
         //melakukan deposit rekening dollar
         cy.log('memilih rekening');
-        Banking.selectaccount('1004');
+        Banking.selectaccount('1005');
         //assertion
         cy.contains('Harry Potter').should('be.visible');
-        cy.contains('Account Number : 1004').should('be.visible');
-        cy.contains('Currency : Dollar').should('be.visible');
+        cy.contains('Account Number : 1005').should('be.visible');
+        cy.contains('Currency : Pound').should('be.visible');
         
         Banking.deposit();
         cy.log('memasukan jumlah');
@@ -181,32 +192,29 @@ describe('Validasi Fitur User di rekening Dollar ', () => {
         cy.log('memasukan jumlah');
         Banking.enterwithdrawl('-1000');
         cy.get('button[type="submit"]').should('not.be.disabled');
-
     });
 
-
-    it('Harus berhasil melakukan withdrawl di rekening Dollar', () => {
+    it('Harus berhasil melakukan withdrawl di rekening Pound', () => {
         cy.log('Memilih user')
         Banking.selectuser('Harry Potter');
         Banking.loginusername();
        
         //melakukan deposit rekening dollar
         cy.log('memilih rekening');
-        Banking.selectaccount('1004');
+        Banking.selectaccount('1005');
         //assertion
         cy.contains('Harry Potter').should('be.visible');
-        cy.contains('Account Number : 1004').should('be.visible');
-        cy.contains('Currency : Dollar').should('be.visible');
+        cy.contains('Account Number : 1005').should('be.visible');
+        cy.contains('Currency : Pound').should('be.visible');
         
         Banking.deposit();
         cy.log('memasukan jumlah');
-        Banking.enteramount('5000');
+        Banking.enteramount('6000');
         Banking.submitdeposit();
         //assertion
         cy.log('muncul assertion');
         cy.contains('span', 'Deposit Successful').should('be.visible');
         
-
         //masuk menu withdrawl
         Banking.withdrawl();
         cy.contains('Amount to be Withdrawn :').should('be.visible');
@@ -218,41 +226,4 @@ describe('Validasi Fitur User di rekening Dollar ', () => {
     });
 
 
-    it('Harus berhasil melakukan Logout di halaman user', () => {
-        cy.log('Memilih user');
-        Banking.selectuser('Harry Potter');
-        Banking.loginusername();
-        //assertion
-        cy.contains('Harry Potter').should('be.visible');
-        cy.contains('Account Number : 1004').should('be.visible');
-        cy.contains('Currency : Dollar').should('be.visible');
-
-        //logouta
-        cy.log('melakukan logout');
-        Banking.Logout();
-        
-        //assertion
-        cy.contains('Your Name :').should('be.visible');
-        cy.get('#userSelect').should('exist').and('be.visible')
-        
-    });
-
-    it('Harus berhasil memvalidasi tombol navigasi Home di halaman user', () => {
-        cy.log('Memilih user');
-        Banking.selectuser('Harry Potter');
-        Banking.loginusername();
-        
-        //assertion
-        cy.contains('Harry Potter').should('be.visible');
-        cy.contains('Account Number : 1004').should('be.visible');
-        cy.contains('Currency : Dollar').should('be.visible');
-
-        cy.log('kembali ke halaman utama')
-        Banking.navigateTohome();
-
-        //assertion
-        cy.get('button[ng-click="home()"]').should('be.visible');
-        cy.get('button[ng-click="customer()"]').should('be.visible');
-        cy.get('button[ng-click="manager()"]').should('be.visible');
-    });
 });
